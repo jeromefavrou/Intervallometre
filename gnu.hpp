409b7cbd6ffa93e17bf1417b3bda34439e087e44
Favrou jérôme <38081092+jeromefavrou@users.noreply.github.com>
2018-09-11 21:19:10 +0200

    Add files via upload

---
+++ gnu.hpp
@@ -0,0 +1,13 @@
+#ifndef GNU_HPP_INCLUDED
+#define GNU_HPP_INCLUDED
+
+class GNU
+{
+ public:
+     GNU(void)
+     {
+     }
+ private:
+};
+
+#endif // GNU_HPP_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "Intervallometre.hpp"
-
+#include "gnu.hpp"
 int main(int argc,char ** argv)
 {
     Intervallometre inter;
