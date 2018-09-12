d099ccaecbf7dcdc7d40344df62be98bf578f3b2
Favrou jérôme <38081092+jeromefavrou@users.noreply.github.com>
2018-09-12 21:41:02 +0200

    Add files via upload

--- raw_import.hpp
+++ raw_import.hpp
@@ -2,21 +2,32 @@
 #define RAW_IMPORT_HPP_INCLUDED
 
 #include <string>
+#include <memory>
 #include <opencv2/opencv.hpp>
 
 template<class T> class RAW
 {
 public:
-    RAW(std::string const & file):m_file(file)
+    RAW(void):m_file(""),m_data(nullptr)
     {
     }
-    RAW(RAW const & cpy):m_file(cpy.get_file()),m_data(cpy.get_data())
+
+    ~RAW()
     {
     }
+
+    RAW(std::string const & file):m_file(file),m_data(nullptr)
+    {
+    }
+
+    RAW(RAW const & cpy):m_file(cpy.get_file()),m_data(std::make_shared<cv::Mat>(cpy.get_data()))
+    {
+    }
+
     RAW operator=(RAW const & cpy)
     {
         this->m_file=cpy.get_file();
-        this->m_data=cpy.get_data();
+        this->m_data=std::make_shared<cv::Mat>(cpy.get_data());
 
         return *this;
     }
@@ -25,14 +36,22 @@ public:
     {
         return this->m_file;
     }
+
     cv::Mat get_data(void)const
     {
-        return this->m_data;
+        return *this->m_data;
     }
+
     cv::Mat & data(void)
+    {
+        return *this->m_data;
+    }
+
+    std::shared_ptr<cv::Mat> get_data(void)
     {
         return this->m_data;
     }
+
 
     bool load(void)
     {
@@ -41,7 +60,7 @@ public:
 
 private:
     std::string m_file;
-    cv::Mat m_data;
+    std::shared_ptr<cv::Mat> m_data;
 };
 
 #endif // RAW_IMPORT_HPP_INCLUDED
